03e47429358545f71f25d3afe6fe8bb042036791
Seth <seth@eventespresso.com>
2013-10-27 09:38:58 -0600

    Fixing open spots and event status variables.

--- index.php
+++ index.php
@@ -57,7 +57,11 @@ function espresso_custom_template_grid(){
 			$externalURL 		= $event->externalURL;
 			$registration_url 	= !empty($externalURL) ? $externalURL : espresso_reg_url($event->id);
 			$event_status 		= event_espresso_get_status($event->id);
-			$open_spots			= get_number_of_attendees_reg_limit($event->id, 'number_available_spaces');
+			if ( ! has_filter( 'filter_hook_espresso_get_num_available_spaces' ) ){
+				$open_spots		= apply_filters('filter_hook_espresso_get_num_available_spaces', $event->id); //Available in 3.1.37
+			}else{
+				$open_spots		= get_number_of_attendees_reg_limit($event->id, 'number_available_spaces');
+			}
 			$link_text 			= __('Register Now!', 'event_espresso');
 
 			//use the wordpress date format.
